92b1fe234773ec94da871be1bc088153a027bcc8
Jeremy Josey <webjeremy@gmail.com>
2023-05-08 20:11:08 -0300

    Added Form

--- website/views.py
+++ website/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html', {})
+    code_list = ['bash', 'c', 'clike', 'cpp', 'csharp', 'css', 'django', 'docker', 'graphql', 'javascript', 'javadoclike',
+                'jsx', 'mongodb', 'php', 'php-extras', 'phpdoc', 'powershell', 'python', 'regex', 'scss', 'sql', 'tsx', 'typescript']
+
+    return render(request, 'home.html', {'code_list': code_list})
 
